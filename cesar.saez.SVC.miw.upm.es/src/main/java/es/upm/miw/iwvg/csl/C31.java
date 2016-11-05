bd0fb8143a2fe4c83ee1d2a8ad9b49a5043bd343
CESAR <CESAR@192.168.1.131>
2016-11-05 14:35:12 +0100

    C31 Create

---
+++ cesar.saez.SVC.miw.upm.es/src/main/java/es/upm/miw/iwvg/csl/C31.java
@@ -0,0 +1,11 @@
+package es.upm.miw.iwvg.csl;
+
+public class C31 {
+    public static String m1(){
+        return "m1";
+    }
+    
+    public static String m2(){
+        return "m2";
+    }
+}
